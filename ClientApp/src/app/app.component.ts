080f67da0131f36352efe3c694121207f69727df
3r1xon <erixonconsoli@hotmail.it>
2021-12-06 23:04:32 +0100

    Sync

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/services/user.service';
-import { WebSocketService } from 'src/services/websocket.service';
 
 
 @Component({
@@ -12,11 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(
     private _user: UserService,
-    private _webSocket: WebSocketService
     ) { }
 
   async ngOnInit() {
     await this._user.authorize();
-    // this._webSocket.openWebSocket();
   }
 }
