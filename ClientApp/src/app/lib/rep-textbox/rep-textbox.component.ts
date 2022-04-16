1345e9497c7c3052cbf1e1a5d4e1a2a7e6f37a29
3r1xon <erixonconsoli@hotmail.it>
2022-04-16 22:56:59 +0200

    Sync

--- ClientApp/src/app/lib/rep-textbox/rep-textbox.component.ts
+++ ClientApp/src/app/lib/rep-textbox/rep-textbox.component.ts
@@ -49,6 +49,8 @@ export class REPTextBoxComponent {
   public send(event) {
     if (this.form.valid && this.enabled) {
       const txt = this.form.value["text"].trim();
+      this.addToHistory(this.form.value["text"]);
+      this.historyIndex = this.textHistory.length;
       this.sendMessage.emit(txt);
       this.form.reset();
       this.trigger = false;
@@ -58,5 +60,47 @@ export class REPTextBoxComponent {
   public uploadIMG() {
 
     this.upload.emit("Upload");
+  }
+
+  private textHistory: Array<string> = [];
+
+  private historyIndex: number;
+
+  addToHistory(text: string) {
+    if (this.textHistory.some(txt => txt == text))
+      return;
+
+    this.textHistory.push(text);
+  }
+
+  setPreviousText() {
+    if (this.textHistory.length == 0)
+      return;
+
+    if (this.historyIndex == null)
+      this.historyIndex = this.textHistory.length - 1;
+    else if (!(this.historyIndex - 1 < 0)) {
+      this.historyIndex--;
+    }
+
+    if (this.textHistory[this.historyIndex]) {
+      if (this.form.value["text"] != null && this.form.value["text"] != "")
+        this.addToHistory(this.form.value["text"]);
+
+      this.setTextValue(this.textHistory[this.historyIndex]);
+    }
+  }
+
+  setNextText() {
+    if (this.textHistory.length == 0 || this.historyIndex == null)
+      return;
+
+    if (this.historyIndex + 1 != this.textHistory.length)
+      this.historyIndex++;
+
+    if (this.form.value["text"] != null && this.form.value["text"] != "")
+      this.addToHistory(this.form.value["text"]);
+
+    this.setTextValue(this.textHistory[this.historyIndex]);
   }
 }
