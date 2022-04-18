19196a1d20b4511a4659bc00728d40fb3f05a03c
3r1xon <erixonconsoli@hotmail.it>
2022-04-18 20:29:38 +0200

    Sync

--- ClientApp/src/app/pages/p-mainpage/p-mainpage.component.ts
+++ ClientApp/src/app/pages/p-mainpage/p-mainpage.component.ts
@@ -5,15 +5,15 @@ import {
   Component,
   OnInit,
 } from '@angular/core';
-import { openLeftNoDestroy, openRightNoDestroy } from 'src/app/lib/rep-animations';
+import { openLeft, openRight } from 'src/app/lib/rep-animations';
 
 
 @Component({
   templateUrl: './p-mainpage.component.html',
   styleUrls: ['./p-mainpage.component.scss'],
   animations: [
-    openLeftNoDestroy("250px", "100ms"),
-    openRightNoDestroy("250px", "100ms")
+    openLeft("100ms", "-250px"),
+    openRight("100ms", "-250px")
   ]
 })
 export class PMainpageComponent implements OnInit {
