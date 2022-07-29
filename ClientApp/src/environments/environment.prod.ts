6949e595582297943637af959dfab6fd081ff890
Erison <76662702+3r1xon@users.noreply.github.com>
2022-07-29 17:15:51 +0200

    Update environment.prod.ts

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: "https://republichat.jasoc.duckdns.org:3030",
-  WEB_SOCKET: "wss://republichat.jasoc.duckdns.org:3030"
+  BASE_URL: "https://republichat.mindrive.ddnfsfree.com:6996",
+  WEB_SOCKET: "wss://republichat.mindrive.ddnsfree.com:6996"
 };
