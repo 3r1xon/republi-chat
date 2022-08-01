efa21ddb638c27689cbab486f35ba9187ab69af9
Erison <76662702+3r1xon@users.noreply.github.com>
2022-08-01 15:13:28 +0200

    Update environment.prod.ts

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: "https://republichat.mindrive.ddnfsfree.com:6996",
+  BASE_URL: "https://republichat.mindrive.ddnsfree.com:9696",
   WEB_SOCKET: "wss://republichat.mindrive.ddnsfree.com:6996"
 };
